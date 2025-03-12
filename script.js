543ab7cb0bc0d47ee5e51c21eac23757bb56772d
Lucasgbbb <lucasgabrielp1470@gmail.com>
2025-03-12 16:46:06 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    const img = document.querySelector("#profile img")
+
+    if(html.classList.contains('light')) {
+
+        img.setAttribute('src', './assents/avatar-light.png')
+
+    } else {
+
+        img.setAttribute('src' , './assents/azul.jpg')
+
+    }
+
+}
